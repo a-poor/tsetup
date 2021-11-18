f032a1e2f9640e3ed79cd1ea65a579872bd66ead
a-poor <45295232+a-poor@users.noreply.github.com>
2021-11-18 10:24:47 -0800

    Updating CLI

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
+import * as Listr from 'listr'
+import * as fs from 'fs'
+import { exec } from "child_process"
+
+const helloWorldFile = `
+export default function greet(name: string = 'World'): string {
+  return "Hello, " + name + "!"
+}
+`
 
 class Tsetup extends Command {
-  static description = 'describe the command here'
+  static description = 'Get a TypeScript off the ground quickly'
 
   static flags = {
     // add --version flag to show CLI version
@@ -15,14 +24,87 @@ class Tsetup extends Command {
 
   static args = [{name: 'file'}]
 
-  async run() {
-    const {args, flags} = this.parse(Tsetup)
+  async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+  async exec(command: string, args: string[] = [], printOutput = false) {
+    await this.delay(1000)
+
+    const sargs = args.reduce((acc, arg) => `${acc} ${arg}`)
+    const cmd = sargs.length > 0 ? `${command} ${sargs}` : `${command}`
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        throw error
+      }
+
+      // Stop if not printing output
+      if (!printOutput) return
+
+      // Print to stderr if it exists
+      if (stderr)
+        this.error(stderr)
+
+      // Print to stdout if it exists
+      if (stdout)
+        this.log(stdout)
+    })
+  }
+
+  async run() {
+    // Capture run params
+    // const {args, flags} = this.parse(Tsetup)
+
+    // Describe the tasks
+    const tasks = new Listr([
+      {
+        title: "Initialize the npm project", // npm init -y
+        task: async () => this.exec('npm', ['init', '-y']),
+      },
+      {
+        title: "Install typescript", // npm i -D typescript
+        task: async () => this.exec('npm', ['i', '-D', 'typescript']),
+      },
+      {
+        title: "Initialize the typescript project", // npm exec -y tsc --init
+        task: async () => this.exec('npm', ['exec', '-y', 'tsc', '--init']),
+      },
+      {
+        title: "Create \"src\" and \"dist\" directories", // mkdir src dist
+        task: async () => this.exec('mkdir', ['-p', 'src', 'dist']),
+      },
+      {
+        title: "Adding placeholder \"src/index.ts\"", // TODO: this
+        task: async () => {
+          await this.exec('touch', ['src/index.ts'])
+          try {
+            fs.writeFileSync("src/index.ts", helloWorldFile)
+          } catch (error) {
+            throw error
+          }
+        },
+      },
+      {
+        title: "Update the tsconfig", // Update tsconfig.json to add outDir and sourceMap
+        skip: () => true,
+        task: async () => this.exec("echo", ["Hello, world"]),
+      },
+      {
+        title: "Install eslint", // npm i -D eslint
+        task: async () => this.exec("npm", ["i", "-D", "eslint"]),
+      },
+      {
+        title: "Set up eslint", // TODO: this
+        task: async () => this.exec("npx", ["eslint", "--init"]),
+      },
+      {
+        title: "Initialize a git repo", // npm init -y
+        task: async () => this.exec('git', ['init']),
+      },
+    ])
+
+    // Run the tasks
+    tasks.run().catch((error: Error) => this.error(error))
   }
 }
 
